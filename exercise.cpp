dd08e13b301fbfafe98def00a8dfde58fbea8337
hellomraza <85604865+hellomraza@users.noreply.github.com>
2023-01-04 12:23:30 +0530

    iiii

---
+++ exercise.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// . Write a C++ program to check two given integers, and return true if one of them is 30 or if their sum is 30.
+
+// bool test(int a, int b)
+// {
+//     if (a == 30 || b == 30)
+//     {
+//         return true;
+//     }
+//     else if (a + b == 30)
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// };
+
+// int main()
+// {
+//     cout << test(30, 0) << endl;
+//     cout << test(25, 5) << endl;
+//     cout << test(20, 30) << endl;
+//     cout << test(20, 25) << endl;
+//     return 0;
+// }
+
+// Write a C++ program to create a new string where 'if' is added to the front of a given string. If the string already begins with 'if', return the string unchanged.
+
+// string addIf(string str)
+// {
+//     if (str.length() > 2 && str.substr(0, 2) == "if")
+//     {
+//         return str;
+//     }
+//     else
+//     {
+//         return "if " + str;
+//     }
+// }
+
+// int main()
+// {
+//     cout << addIf("if else") << endl;
+//     cout << addIf("else") << endl;
+// }
+
+// Write a C++ program to exchange the first and last characters in a given string and return the new string.
+
+int main()
+{
+    string str = "abcd";
+}
